e25e88ba89378003377e7e5a71876c1ba2d10f49
Samuel Bassey <37877992+ctrlfix@users.noreply.github.com>
2024-03-12 16:58:37 +0100

    Add files via upload

---
+++ sample/js/showMenu.js
@@ -0,0 +1,12 @@
+
+function showMenu(){
+	var x = document.getElementById("sub-menus");
+
+	if(x.style.display==="block"){
+		x.style.display="none";
+	} else{
+		x.style.display = "block";
+	}
+}
+
+
